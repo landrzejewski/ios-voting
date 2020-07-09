b3cdef57e5630f70043c2f35948f9754efe81a52
landrzejewski <landrzejewski.poczta@gmail.com>
2020-07-09 11:03:52 +0200

    Initial Commit

---
+++ Voting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voting
+//
+//  Created by Łukasz Andrzejewski on 09/07/2020.
+//  Copyright © 2020 Inbright Łukasz Andrzejewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
